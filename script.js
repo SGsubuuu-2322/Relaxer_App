48f0e5d29066e86810a947de20244cee7fb64d54
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-30 19:02:58 +0530

    Added the script for adding grow and shrink animation onto the container...

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ breatheAnimation();
 
 function breatheAnimation() {
   text.innerText = "Breathe In";
+  container.className = "container grow";
 
   setTimeout(() => {
     text.innerText = "Hold";
 
     setTimeout(() => {
       text.innerText = "Breathe Out";
+      container.className = "container shrink";
     }, holdTime);
   }, breatheTime);
 }
+
+setInterval(breatheAnimation, totalTime);
